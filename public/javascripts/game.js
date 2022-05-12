4e789ac2706b34b614d11935e846f43637317575
Xavier Flix <morphx666@users.noreply.github.com>
2022-05-12 01:25:04 -0400

    When winning set userMines to 0

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -89,6 +89,7 @@ function createBoard() {
                     if(t == cells.length) {
                         console.log("You Win!");
                         gameState = 2;
+                        userMines = 0;
                         stopTimer();
                         cells.forEach(c => {
                             if(c.mine) {
